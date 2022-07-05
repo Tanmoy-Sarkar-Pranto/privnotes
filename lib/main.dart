c21f3966a01f923ce045d3ce1e5a28e1a9b1a72a
Tanmoy-Sarkar-Pranto <ptanmoysarkar@gmail.com>
2022-07-05 12:22:52 +0600

    small change in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:privnotes/views/login_view.dart';
 import 'package:privnotes/views/register_view.dart';
+import 'package:privnotes/views/verify_email_view.dart';
 
 import 'firebase_options.dart';
 
@@ -34,15 +36,25 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // final emailVerified = user?.emailVerified ?? false;
-            // if (user?.emailVerified ?? false) {
-            //   return const Text("Done");
-            // } else {
-            //   print(user);
-            //   return const EmailVerifyView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print("Email is verified");
+              } else {
+                return const EmailVerifyView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text("Done");
+          // final emailVerified = user?.emailVerified ?? false;
+          // if (user?.emailVerified ?? false) {
+          //   return const Text("Done");
+          // } else {
+          //   print(user);
+          //   return const EmailVerifyView();
+          // }
+          //return const LoginView();
           default:
             return const Text("Loading..");
         }
