8f4123fe8989af5766fa816d4c24c18ae2febdb0
Tanmoy-Sarkar-Pranto <ptanmoysarkar@gmail.com>
2022-07-05 12:09:06 +0600

    verify_email_view.dart and navigation added

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class EmailVerifyView extends StatefulWidget {
+  const EmailVerifyView({Key? key}) : super(key: key);
+
+  @override
+  State<EmailVerifyView> createState() => _EmailVerifyViewState();
+}
+
+class _EmailVerifyViewState extends State<EmailVerifyView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Email Verify"),
+      ),
+      body: Column(
+        children: [
+          const Text("Please Verify Your Email"),
+          TextButton(
+            onPressed: () async {
+              final user = FirebaseAuth.instance.currentUser;
+              await user?.sendEmailVerification();
+            },
+            child: const Text("Send E-Mail Verification"),
+          ),
+        ],
+      ),
+    );
+  }
+}
